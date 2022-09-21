51bc998e6b6cb6ac9b05b6d36192af0c64894caf
«Helena» <moiseevaelena44@yandex.ru>
2022-09-21 16:43:38 +0300

    Page Color & Types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import 'jquery'
 import './main.scss'
 
+
+
+
 console.log('- Did Joffrey agree?\n- He did. He also said "I love using \\n".');
+
+let booksCount = 19; // Количество книг, прочтённых за год
+let result; // Сюда запишем результат сравнения booksCount с эталонным значением
+
+// Сравним с помощью условной конструкции if
+if (booksCount > 15) {
+  result = 'План на год выполнен!';
+} else {
+  result = 'Читать и ещё раз читать';
+}
+
+console.log(result);
+
+// А теперь с помощью тернарного оператора
+result = (booksCount > 15) ? 'План на год выполнен!' : 'Читать и ещё раз читать';
+
+console.log(result);
--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,17 @@ module.exports = {
   mode: 'development',
   entry: './index.js',
   output: {
-  	filename: '[name].[contenthash].js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'docs')
   },
   
   module: {
-  	rules: [
-  	
-  		{
-			test: /\.pug$/,
-			use: ['pug-loader?pretty=true']
-  		},
+    rules: [
+    
+      {
+      test: /\.pug$/,
+      use: ['pug-loader?pretty=true']
+      },
 
   {
       test: /\.(sass|scss)$/,
@@ -33,44 +33,70 @@ module.exports = {
           ] 
       },
 
-		{
-  	    test: /\.(png|jpg|svg|tiff|gif)$/,
-  	    use: ['file-loader'],
-		},
+    {
+        test: /\.(png|jpg|svg|tiff|gif)$/,
+        use: ['file-loader'],
+    },
 
-		{
-  	    test: /\.(ttf|woff|woff2|eot|otf)$/,
-  	    use: ['file-loader']
-		}
+    {
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        use: ['file-loader']
+    }
 
-  	]
+    ]
   },
 
 devServer: {
-	port: 4200,
-	open: true
+  port: 4200,
+  open: true
 },
 
 plugins: [
-	new HtmlWebpackPlugin({
-		template: './index.pug' 
-	}),
+  new HtmlWebpackPlugin({
+    template: './pages/starting-page/starting-page.pug',
+    filename: 'index.html' 
+  }),
 
-	 	new CopyWebpackPlugin({
-	patterns: [
-		{
-		from: path.resolve(__dirname, 'src/assets/img/pug'),
-		to: path.resolve(__dirname, 'docs')
-		}
-  		]
-	}),
+new HtmlWebpackPlugin({
+    template: './pages/landing-page/landing-page.pug',
+    filename: 'landing-page.html' 
+  }),
 
-	 	new CleanWebpackPlugin(),
+new HtmlWebpackPlugin({
+    template: './pages/ui-kit-cards/ui-kit-cards.pug',
+    filename: 'ui-kit-cards.html' 
+  }),
 
-	 	new MiniCssExtractPlugin({
-	    filename: '[name].[contenthash].css'
-	})	
+  new HtmlWebpackPlugin({
+    template: './pages/ui-kit-colors-type/ui-kit-colors-type.pug',
+    filename: 'ui-kit-colors-type.html' 
+  }),
 
-	]
+  new HtmlWebpackPlugin({
+    template: './pages/ui-kit-headers-footers/ui-kit-headers-footers.pug',
+    filename: 'ui-kit-headers-footers.html' 
+  }),
+
+  new HtmlWebpackPlugin({
+    template: './pages/ui-kit-form-elements/ui-kit-form-elements.pug',
+    filename: 'ui-kit-form-elements.html' 
+  }),
+
+    new CopyWebpackPlugin({
+  patterns: [
+    {
+    from: path.resolve(__dirname, 'src/assets/img/pug'),
+    to: path.resolve(__dirname, 'docs')
+    }
+      ]
+  }),
+
+    new CleanWebpackPlugin(),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+  })  
+
+  ]
 }
 
